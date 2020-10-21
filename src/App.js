c5dcb8869c30dfe1c3ebf7dba25c57de6bd7ce86
Lara Samuelson <lara.samuelson@gmail.com>
2020-10-21 11:54:02 -0700

    buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import STORE from './STORE'
 import List from './List';
 import './App.css';
 
@@ -13,28 +14,79 @@ class MyComponent extends React.Component {
   }
 */
 
-
-
 /*To render a component as a child in a React component, you include the component name written as a custom HTML tag in the JSX
 this is done below with <List/>.
 When React encounters a custom HTML tag that references another component (a component name wrapped in < /> like in this example), 
 it renders the markup for that component in the location of the tag.
 */
-
-
+const newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+}
+function omit(obj, keyToOmit) {
+  return Object.entries(obj).reduce(
+    (newObj, [key, value]) =>
+        key === keyToOmit ? newObj : {...newObj, [key]: value},
+    {}
+  );
+}
 
 class App extends Component {
-  static defaultProps  = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
+  state = {
+    store: STORE,
   };
+
+  handleDeleteCard = (cardId) => {
+    const { lists, allCards } = this.state.store;
+
+    const newLists = lists.map(list => ({
+      ...list,
+      cardIds: list.cardIds.filter(id => id !== cardId)
+    }));
+
+    const newCards = omit(allCards, cardId);
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: newCards
+      }
+    })
+  };
+
+  handleAddCard = (listId) => {
+    const newCard = newRandomCard()
+
+    const newLists = this.state.store.lists.map(list => {
+      if (list.id === listId) {
+	return {
+          ...list,
+          cardIds: [...list.cardIds, newCard.id]
+        };
+      }
+      return list;
+    })
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: {
+          ...this.state.store.allCards,
+          [newCard.id]: newCard
+        }
+      }
+    })
+  };	  
 
 
 
   render() { 
-      const { store } = this.props
+      const { store } = this.state
       return (
       <main className='App'>
         <header className="App-header">
@@ -44,8 +96,11 @@ class App extends Component {
           {store.lists.map(list => (
             <List
              key={list.id}
+             id={list.id}
              header={list.header}
              cards={list.cardIds.map(id => store.allCards[id])}
+             onClickDelete={this.handleDeleteCard}
+             onClickAdd={this.handleAddCard}
             />
           ))}
         </div>
--- src/List.js
+++ src/List.js
@@ -13,14 +13,17 @@ export default function List(props) {
           {props.cards.map((card) =>
             <Card
               key={card.id}
+              id={card.id}
               title={card.title}
               content={card.content}
+              onClickDelete={props.onClickDelete}
               
             />
           )}
           <button
             type='button'
             className='List-add-button'
+            onClick={() => props.onClickAdd(props.id)}
           >
             + Add Random Card
           </button>
@@ -29,7 +32,9 @@ export default function List(props) {
     )
   }
   
-
+  List.defaultProps = {
+    onClickAdd: () => {},
+  }
 
 
 
